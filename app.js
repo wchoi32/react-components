015733bca6019d848ff1146e39f2d4b151c6991f
WooSung Choi <johnwschoi@gmail.com>
2018-01-31 12:20:34 -0800

    Done with React Exercise

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
 // TODO
+class GroceryListItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            done: false
+        };
+    }
+
+    onListItemHover() {
+        this.setState({
+            done: !this.state.done
+        });
+    }
+
+
+    render() {
+        var style = {
+            'font-weight': this.state.done ? 'bold': 'normal'
+        };
+
+        return (
+            <li style={style} onMouseOver={this.onListItemHover.bind(this)} onMouseOut ={this.onListItemHover.bind(this)}>{this.props.item}</li>
+        );
+    }
+}
+
+var GroceryList = (props) => (
+    <ul>
+        {props.items.map(item =>
+            <GroceryListItem item={item} />
+        )}
+    </ul>
+);
+
+var App = () => (
+    <div>
+        <GroceryList items={['beer', 'coffee', 'apple']}/>
+    </div>
+);
+
+ReactDOM.render (
+    <App />, 
+    document.getElementById('app')
+);
+
+// {props.items.map(item =>
+//     <GroceryListItem items={item} />
+// )}
